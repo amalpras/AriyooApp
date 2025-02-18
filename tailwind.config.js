b7e351d33b77687b0e2306baa66ee2898baf5844
Vanessa Tojan <dev.wenmax@espcflorida.com>
2025-02-18 18:46:06 +0530

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Ensure Tailwind scans all Angular templates and components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
